52cbd8405d0e1679c25c487c3ae36cb8b59265db
Michael Dunning <mdunning@slac.stanford.edu>
2023-06-28 18:31:49 -0700

    Remove

--- boy/macros/Channel_macro.js
+++
@@ -1,7 +0,0 @@
-importPackage(Packages.org.csstudio.opibuilder.scriptUtil);
-
-var macroInput = DataUtil.createMacrosInput(true);
-var pv = PVUtil.getString(pvArray[0]);
-
-macroInput.put("ADQChannel", pv);
-widgetController.setPropertyValue("macros", macroInput);
--- boy/macros/SerialNumber_macro.js
+++
@@ -1,7 +0,0 @@
-importPackage(Packages.org.csstudio.opibuilder.scriptUtil);
-
-var macroInput = DataUtil.createMacrosInput(true);
-var pv = PVUtil.getString(pvArray[0]);
-
-macroInput.put("ADQSerialNumber", pv);
-widgetController.setPropertyValue("macros", macroInput);
